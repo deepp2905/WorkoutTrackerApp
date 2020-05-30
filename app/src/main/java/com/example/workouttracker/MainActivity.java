ae7a2cefac75e966559c6b8258aa8108238c7a5d
Deep Patel <deephemapatel@gmail.com>
2020-05-31 00:18:56 +0530

    Added roundbutton and colour scheme for the app

--- app/src/main/java/com/example/workouttracker/MainActivity.java
+++ app/src/main/java/com/example/workouttracker/MainActivity.java
@@ -6,6 +6,28 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    /*COLOR PALLETE
+    50
+    #fafafa
+    100
+    #f5f5f5
+    200
+    #eeeeee
+    300
+    #e0e0e0
+    400
+    #bdbdbd
+    500
+    #9e9e9e
+    600
+    #757575
+    700
+    #616161
+    800
+    #424242
+    900
+    #212121*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
